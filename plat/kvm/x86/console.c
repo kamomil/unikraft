af5f2b44d4f6a26a377b2027bbe0e92673f3aeb4
kamomil <dafna3@gmail.com>
2018-04-02 20:36:13 +0300

    plat/kvm: Change the console to print to vga

--- plat/kvm/x86/console.c
+++ plat/kvm/x86/console.c
@@ -45,14 +45,71 @@
 #define DLAB 0x80
 #define PROT 0x03 /* 8N1 (8 bits, no parity, one stop bit) */
 
+enum vga_color {
+        COLOR_BLACK = 0,
+        COLOR_BLUE = 1,
+        COLOR_GREEN = 2,
+        COLOR_CYAN = 3,
+        COLOR_RED = 4,
+        COLOR_MAGENTA = 5,
+        COLOR_BROWN = 6,
+        COLOR_LIGHT_GREY = 7,
+        COLOR_DARK_GREY = 8,
+        COLOR_LIGHT_BLUE = 9,
+        COLOR_LIGHT_GREEN = 10,
+        COLOR_LIGHT_CYAN = 11,
+        COLOR_LIGHT_RED = 12,
+        COLOR_LIGHT_MAGENTA = 13,
+        COLOR_LIGHT_BROWN = 14,
+        COLOR_WHITE = 15,
+};
+
+uint8_t make_color(enum vga_color fg, enum vga_color bg) {
+        return fg | bg << 4;
+}
+
+uint16_t make_vgaentry(char c, uint8_t color) {
+        uint16_t c16 = c;
+        uint16_t color16 = color;
+        return c16 | color16 << 8;
+}
+
+/*
+size_t strlen(const char* str) {
+        size_t ret = 0;
+        while ( str[ret] != 0 )
+                ret++;
+        return ret;
+}
+*/
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+
 void _libkvmplat_init_console(void)
 {
-	uk_printd(DLVL_INFO, "initializing serial\n");
-	outb(COM1_INTR, 0x00);  /* Disable all interrupts */
-	outb(COM1_CTRL, DLAB);  /* Enable DLAB (set baudrate divisor) */
-	outb(COM1_DIV_LO, 0x01);/* Set div to 1 (lo byte) 115200 baud */
-	outb(COM1_DIV_HI, 0x00);/*              (hi byte) */
-	outb(COM1_CTRL, PROT);  /* Set 8N1, clear DLAB */
+	//uk_printd(DLVL_INFO, "initializing serial\n");
+	//outb(COM1_INTR, 0x00);  /* Disable all interrupts */
+	//outb(COM1_CTRL, DLAB);  /* Enable DLAB (set baudrate divisor) */
+	//outb(COM1_DIV_LO, 0x01);/* Set div to 1 (lo byte) 115200 baud */
+	//outb(COM1_DIV_HI, 0x00);/*              (hi byte) */
+	//outb(COM1_CTRL, PROT);  /* Set 8N1, clear DLAB */
+
+        terminal_row = 0;
+        terminal_column = 0;
+        terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+        terminal_buffer = (uint16_t*) 0xB8000;
+        for (size_t y = 0; y < VGA_HEIGHT; y++) {
+                for (size_t x = 0; x < VGA_WIDTH; x++) {
+                        const size_t index = y * VGA_WIDTH + x;
+                        terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+                }
+        }
 }
 
 int ukplat_coutd(const char *str, unsigned int len)
@@ -65,19 +122,21 @@ static int serial_tx_empty(void)
 	return inb(COM1_STATUS) & 0x20;
 }
 
-static void serial_write(char a)
+static void serial_write(char c, uint8_t color, size_t x, size_t y)
 {
-	while (!serial_tx_empty())
-		;
-
-	outb(COM1_DATA, a);
+        const size_t index = y * VGA_WIDTH + x;
+        terminal_buffer[index] = make_vgaentry(c, color);
 }
 
-static void serial_putc(char a)
+static void serial_putc(char c)
 {
-	if (a == '\n')
-		serial_write('\r');
-	serial_write(a);
+        serial_write(c, terminal_color, terminal_column, terminal_row);
+        if (++terminal_column == VGA_WIDTH) {
+                terminal_column = 0;
+                if (++terminal_row == VGA_HEIGHT) {
+                        terminal_row = 0;
+                }
+        }
 }
 
 static int serial_rx_ready(void)
